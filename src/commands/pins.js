d6dc0114fa9bba5bd04fd53fdb33cbc12721b6f0
laforcem <90416865+laforcem@users.noreply.github.com>
2022-10-24 20:59:31 -0600

    formatting

--- src/commands/pins.js
+++ src/commands/pins.js
@@ -1,18 +1,21 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('pins')
-		.setDescription('Tells you the total number of pins on the channel'),
+		.setName("pins")
+		.setDescription("Tells you the total number of pins on the channel"),
 	async execute(interaction, client) {
-		var channel = client.channels.cache.get(interaction.channelId)
-		channel.messages.fetchPinned().then(async pins => {
-			await interaction.reply(`There is ${pins.size} pin${(pins.size > 1) ? 's' : ''} on <#${interaction.channelId}>`);
-		})
-		
+		var channel = client.channels.cache.get(interaction.channelId);
+		channel.messages.fetchPinned().then(async (pins) => {
+			await interaction.reply(
+				`There is ${pins.size} pin${pins.size > 1 ? "s" : ""} on <#${
+					interaction.channelId
+				}>`
+			);
+		});
 	},
 };
 
-function getPins(channel){
-	return channel.messages.fetchPinned()
+function getPins(channel) {
+	return channel.messages.fetchPinned();
 }
